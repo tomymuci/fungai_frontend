ac1a73c48cbf8af0c2a0e31340f6a3e61e885fe6
Tomy Muci <tomymuci@gmail.com>
2022-12-08 15:10:36 +0100

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -136,7 +136,7 @@ if button and uploaded_file is not None:
 
         predicted_genus = max(genuses, key=genuses.get)
         
-        st.markdown(f'<p style="font-family:Courier; color:Blue; font-size: 20px;">{predicted_genus}</p>',unsafe_allow_html=True)
+        st.markdown(f'<p style="font-family:sans serif arvo; font-size: 50px;">{predicted_genus}</p>',unsafe_allow_html=True)
 
         
 
